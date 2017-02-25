0a7c26f6e93f6e73972e25558a200b1d73681370
Nathan Glikman <Nathan Glikman>
2017-02-25 14:26:09 -0500

    Changed screen size to 1440x960

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Main
 {
     public static void main(String[] args)
     {
-	JFrame frame = new JFrame();
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setContentPane(new Debug());
-	frame.pack();
-	frame.setVisible(true);
+		JFrame frame = new JFrame();
+		
+		int width = 1440;
+		int height = 960;
+	
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMaximumSize(new Dimension(width, height));
+		frame.setMinimumSize(new Dimension(width, height));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(new Debug());
+		frame.setResizable(false);
+		frame.pack();
+		frame.setVisible(true);
     }
 }
