5a38c40f977a1b034dc6c61da6ac2b9f46382ea7
Nathan Glikman <Nathan Glikman>
2017-02-25 17:05:19 -0500

    Added BufferedImageLoader for loading images

--- src/RoadNode.java
+++ src/RoadNode.java
@@ -1,45 +1,45 @@
 import java.util.List;
 
-public class RoadNode
+public class RoadNode 
 {
-    public class Connection
-    {
-	RoadNode endpoint;
-	int length;
-	float speed;
-	List<Car> incoming;
-
-	public Connection(RoadNode endpoint, int length, float speed)
+	public class Connection 
 	{
-	    this.endpoint = endpoint;
-	    this.length = length;
-	    this.speed = speed;
+		RoadNode endpoint;
+		int length;
+		float speed;
+		List<Car> incoming;
+
+		public Connection(RoadNode endpoint, int length, float speed) 
+		{
+			this.endpoint = endpoint;
+			this.length = length;
+			this.speed = speed;
+		}
 	}
-    }
 
-    Connection[] connections;
-    
-    int x, y;
+	Connection[] connections;
 
-    public RoadNode(int x, int y)
-    {
-	connections = new Connection[4];
-	this.x = x;
-	this.y = y;
-    }
+	int x, y;
 
-    public void addConnection(int direction, RoadNode endpoint, int length, float speed)
-    {
-	connections[direction] = new Connection(endpoint, length, speed);
-	endpoint.connections[(direction + 2) % 4] = new Connection(this, length, speed); 
-    }
-    
-    public void die() 
-    {
-	for(int i = 0; i < 4; i++) 
+	public RoadNode(int x, int y) 
 	{
-	    if(connections[i] == null) continue;
-	    connections[i].endpoint.connections[(i + 2) % 4] = null;
+		connections = new Connection[4];
+		this.x = x;
+		this.y = y;
 	}
-    }
+
+	public void addConnection(int direction, RoadNode endpoint, int length, float speed) 
+	{
+		connections[direction] = new Connection(endpoint, length, speed);
+		endpoint.connections[(direction + 2) % 4] = new Connection(this, length, speed);
+	}
+
+	public void die() 
+	{
+		for (int i = 0; i < 4; i++) {
+			if (connections[i] == null)
+				continue;
+			connections[i].endpoint.connections[(i + 2) % 4] = null;
+		}
+	}
 }
--- src/World.java
+++ src/World.java
@@ -4,70 +4,70 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
-public class World
+public class World 
 {
-    List<RoadNode> intersections;
-    List<Spawner> spawners;
-    Random rand;
+	List<RoadNode> intersections;
+	List<Spawner> spawners;
+	Random rand;
 
-    public World()
-    {
-	intersections = new ArrayList<>();
-	spawners = new ArrayList<>();
-	rand = new Random();
-    }
+	public World() 
+	{
+		intersections = new ArrayList<>();
+		spawners = new ArrayList<>();
+		rand = new Random();
+	}
 
-    public void populateRoads(int width, int height)
-    {
-	System.out.println(width);
-	RoadNode node = new RoadNode(0, 0);
-	populate(node, width, height);
-	for (int i = 0; i < 5 && intersections.size() > 1; i++)
+	public void populateRoads(int width, int height) 
 	{
-	    int index = rand.nextInt(intersections.size());
-	    RoadNode removed = intersections.remove(index);
-	    removed.die();
+		System.out.println(width);
+		RoadNode node = new RoadNode(0, 0);
+		populate(node, width, height);
+		for (int i = 0; i < 5 && intersections.size() > 1; i++) 
+		{
+			int index = rand.nextInt(intersections.size());
+			RoadNode removed = intersections.remove(index);
+			removed.die();
+		}
 	}
-    }
 
-    private RoadNode containsPosition(int x, int y)
-    {
-	for (RoadNode node : intersections)
+	private RoadNode containsPosition(int x, int y) 
 	{
-	    if (x == node.x && y == node.y)
-		return node;
+		for (RoadNode node : intersections) 
+		{
+			if (x == node.x && y == node.y)
+				return node;
+		}
+		return null;
 	}
-	return null;
-    }
 
-    private void populate(RoadNode current, int width, int height)
-    {
-	intersections.add(current);
-	int x = current.x;
-	int y = current.y;
-	if (x >= width || y >= height)
-	    return;
-	RoadNode right = containsPosition(x + 10, y);
-	if (right == null)
+	private void populate(RoadNode current, int width, int height) 
 	{
-	    RoadNode next = new RoadNode(x + 10, y);
-	    current.addConnection(0, next, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
-	    populate(next, width, height);
+		intersections.add(current);
+		int x = current.x;
+		int y = current.y;
+		if (x >= width || y >= height)
+			return;
+		RoadNode right = containsPosition(x + 10, y);
+		if (right == null) 
+		{
+			RoadNode next = new RoadNode(x + 10, y);
+			current.addConnection(0, next, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
+			populate(next, width, height);
+		}
+		else 
+		{
+			current.addConnection(0, right, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
+		}
+		RoadNode down = containsPosition(x, y + 10);
+		if (down == null)
+		{
+			RoadNode next = new RoadNode(x, y + 10);
+			current.addConnection(3, next, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
+			populate(next, width, height);
+		} 
+		else 
+		{
+			current.addConnection(3, down, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
+		}
 	}
-	else 
-	{
-	    current.addConnection(0, right, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
-	}
-	RoadNode down = containsPosition(x, y + 10);
-	if (down == null)
-	{
-	    RoadNode next = new RoadNode(x, y + 10);
-	    current.addConnection(3, next, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
-	    populate(next, width, height);
-	}
-	else 
-	{
-	    current.addConnection(3, down, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
-	}
-    }
 }
