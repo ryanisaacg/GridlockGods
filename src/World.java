ad23d75fe5515458327a600a15879faea8fff527
Ryan Goldstein <ryanisaacg@gmail.com>
2017-02-25 16:47:04 -0500

    Added actual generation

--- src/World.java
+++ src/World.java
@@ -1,4 +1,6 @@
+import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
@@ -17,28 +19,55 @@ public class World
 
     public void populateRoads(int width, int height)
     {
-	RoadNode node = new RoadNode(rand.nextInt(5), rand.nextInt(5));
+	System.out.println(width);
+	RoadNode node = new RoadNode(0, 0);
 	populate(node, width, height);
+	for (int i = 0; i < 5 && intersections.size() > 1; i++)
+	{
+	    int index = rand.nextInt(intersections.size());
+	    RoadNode removed = intersections.remove(index);
+	    removed.die();
+	}
+    }
+
+    private RoadNode containsPosition(int x, int y)
+    {
+	for (RoadNode node : intersections)
+	{
+	    if (x == node.x && y == node.y)
+		return node;
+	}
+	return null;
     }
 
     private void populate(RoadNode current, int width, int height)
     {
 	intersections.add(current);
-	for (int i = 0; i < 4; i++)
+	int x = current.x;
+	int y = current.y;
+	if (x >= width || y >= height)
+	    return;
+	RoadNode right = containsPosition(x + 10, y);
+	if (right == null)
 	{
-	    if (rand.nextBoolean())
-	    {
-		int x = current.x;
-		int y = current.y;
-		int roadLength = rand.nextInt(100) + 25;
-		if (i % 2 == 0)
-		    x += roadLength * (i - 1);
-		else
-		    y += roadLength * (i - 2);
-		if(x >= width || y >= height)
-		    continue; //Don't generate nodes outside of valid space
-		current.addConnection(i, new RoadNode(x, y), roadLength, (float)Math.min(rand.nextFloat() + 0.5, 1));
-	    }
+	    RoadNode next = new RoadNode(x + 10, y);
+	    current.addConnection(0, next, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
+	    populate(next, width, height);
+	}
+	else 
+	{
+	    current.addConnection(0, right, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
+	}
+	RoadNode down = containsPosition(x, y + 10);
+	if (down == null)
+	{
+	    RoadNode next = new RoadNode(x, y + 10);
+	    current.addConnection(3, next, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
+	    populate(next, width, height);
+	}
+	else 
+	{
+	    current.addConnection(3, down, 10, (float) Math.min(rand.nextFloat() + 0.5, 1));
 	}
     }
 }
