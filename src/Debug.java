e609a3b78fddcab3d7940d1e12d333415f96afdf
Ryan Goldstein <ryanisaacg@gmail.com>
2017-02-25 16:45:55 -0500

    Added road rendering to the debug

--- src/Debug.java
+++ src/Debug.java
@@ -5,6 +5,7 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+
 public class Debug extends JPanel
 {
     private static final long serialVersionUID = 1L;
@@ -26,8 +27,21 @@ public class Debug extends JPanel
 	Graphics2D g2d = (Graphics2D)g;
 	for(RoadNode node : world.intersections) 
 	{
+	    if(node.connections[0] != null) {
+		g2d.setColor(Color.GRAY);
+		g2d.fillRect(node.x * 8 + 8, node.y * 8, 80, 8);
+	    }
+	    if(node.connections[3] != null) {
+		g2d.setColor(Color.GRAY);
+		g2d.fillRect(node.x * 8, node.y * 8 + 8, 8, 80);
+	    }
+	}
+	for(RoadNode node : world.intersections) 
+	{
 	    g2d.setColor(Color.RED);
 	    g2d.fillRect(node.x * 8, node.y * 8, 8, 8);
+	    if(node.x * 8 > 640)
+		System.out.println(node.x * 8);
 	}
     }
 }
