af8274cb988b06d47b7fcd351bf0f986127bc078
Ryan Goldstein <ryanisaacg@gmail.com>
2017-02-26 07:21:46 -0500

    Added a traffic update loop

--- src/World.java
+++ src/World.java
@@ -5,33 +5,36 @@ import java.util.Random;
 public class World
 {
     List<TrafficLight> trafficLights;
-	List<RoadNode> intersections;
+    List<RoadNode> intersections;
     List<Spawner> spawners;
     List<Car> cars;
     Random rand;
 
     public World()
     {
-		trafficLights = new ArrayList<>();
-    	intersections = new ArrayList<>();
-		spawners = new ArrayList<>();
-		cars = new ArrayList<>();
-		rand = new Random();
+	trafficLights = new ArrayList<>();
+	intersections = new ArrayList<>();
+	spawners = new ArrayList<>();
+	cars = new ArrayList<>();
+	rand = new Random();
     }
-    
+
     public boolean carFree(int x, int y)
     {
-	for(Car car : cars)
-	    if(car.getTileX() == x && car.getTileY() == y)
+	for (Car car : cars)
+	    if (car.getTileX() == x && car.getTileY() == y)
 		return false;
 	return true;
     }
-    
+
     public void update()
     {
 	for(Spawner spawn : spawners)
 	    if(rand.nextFloat() <= spawn.spawnWeight && carFree(spawn.entrypoint.x, spawn.entrypoint.y))
 		cars.add(new Car(spawn.entrypoint, intersections.get(rand.nextInt(intersections.size()))));
+	for(RoadNode node : intersections)
+	    if(node.light != null)
+		node.light.update();
 	for(int i = 0; i < cars.size(); i++)
 	{
 	    if(cars.get(i).update())
