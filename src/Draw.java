b2dea5b82a2a071fae5c2477594580e477204de3
Nathan Glikman <Nathan Glikman>
2017-02-26 00:31:32 -0500

    Changed cars from blue to graphic and they go in right direction

--- src/Draw.java
+++ src/Draw.java
@@ -18,12 +18,30 @@ public class Draw extends JPanel
     
     private BufferedImage building;
     private BufferedImage[][] street1 = new BufferedImage[4][4];
+    private BufferedImage[][] carPic = new BufferedImage[2][2];
     
     public Draw()
     {
     	world = new World();
     	world.populateRoads(WIDTH, HEIGHT);
     	this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    	
+    	new Thread(() -> 
+    	{
+    	    while (true)
+    	    {
+    	    	world.update();
+    	    	repaint();
+    	    	try
+    	    	{
+    	    		Thread.sleep(16);
+    	    	} 
+    	    	catch (InterruptedException e)
+    	    	{
+    	    		e.printStackTrace();
+    	    	}	
+    	    }
+    	}).start();
     	
     	BufferedImageLoader loader = new BufferedImageLoader();
     	
@@ -34,6 +52,7 @@ public class Draw extends JPanel
     	
     	SpriteSheet ss = new SpriteSheet(streetTilesFile);
     	SpriteSheet ssBuilding = new SpriteSheet(buildingFile);
+    	SpriteSheet ssCar = new SpriteSheet(carFile);
     	
     	for (int i = 0; i < 4; i++) 
     	{
@@ -43,12 +62,20 @@ public class Draw extends JPanel
     		}
     	}
     	building = ssBuilding.grabImage(1, 1, 32, 32);
+    	for (int i = 0; i < 2; i++) 
+    	{
+    		for (int j = 0; j < 2; j++) 
+    		{
+    			carPic[i][j] = ssCar.grabImage(i+1, j+1, 32, 32);
+    		}
+    	}
     }
     
     @Override
     public void paint(Graphics g) 
     {
-		Graphics2D g2d = (Graphics2D)g;
+		super.paint(g);
+    	Graphics2D g2d = (Graphics2D)g;
 		
 		for (int x = 0; x < WIDTH * 3; x += 32) 
 		{
@@ -86,6 +113,26 @@ public class Draw extends JPanel
 		    g2d.fillOval(node.x * 16 + 10, node.y * 16 + 10, 8, 8);
 		    if(node.x * 8 > 640)
 		    	System.out.println(node.x * 8);
+		}
+		for (Car car : world.cars)
+		{
+		    g2d.setColor(Color.BLUE);
+		    if(car.path.size() <= 1)
+		    	continue;
+		    RoadNode.Connection target = car.target();
+		    RoadNode start = car.path.get(0);
+		    RoadNode next = car.path.get(1);
+		    int x = ((int) ((next.x - start.x) * (car.progress / target.length) + start.x) * 16);
+		    int y = ((int) ((next.y - start.y) * (car.progress / target.length) + start.x) * 16);
+		    
+		    if (next.x - start.x > 0)
+		    	g2d.drawImage(carPic[0][0], x, y, null);
+		    if (next.x - start.x < 0)
+		    	g2d.drawImage(carPic[1][1], x, y, null);
+		    if (next.y - start.y > 0)
+		    	g2d.drawImage(carPic[0][1], x, y, null);
+		    if (next.y - start.y < 0)
+		    	g2d.drawImage(carPic[1][0], x, y, null);
 		}
     }
 }
